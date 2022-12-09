c0779308b5cd588b93dca9e9ed49bf34205d230d
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-09 23:59:19 +0100

    Added Car class

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,10 +1,3 @@
-/*
-Napravite klase Car, Van i Limo koje nasljeduje Vehicle. Dodatno, Car sadrzi atribut
-   carType (String), Van sadrzi atribut height (double) i noOfSeats (int), a Limo sadrzi atribute
-   length (double) te boolean varijable miniBar i sunRoof. Napravite klase PassengerVan i CargoVan
-   koje nasljeduje klasu Van, a dodatno imaju privatne atribute noOfPassenger (int, za PassengerVan)
-   tj. maxLoad (double, u kg, za CargoVan). Napravite get i set metode za svaki atribut svih klasa.
- */
 class Car(regNo: String, model: String, year: Int, price: Double, private var carType: String): Vehicle(regNo, model, year, price) {
     fun setCarType(carType: String) {
         this.carType = carType
