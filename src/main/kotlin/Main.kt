422f86a3b65215f0e3ea2d165d41fffd59cb0801
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-10 00:00:23 +0100

    Implemented and tested Car, Van, Limo, CargoVan and PassengerVan class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 fun main(args: Array<String>) {
     val v = Car("DA1234AA", "Renault Clio", 2001, 20.0, "coupe")
+    val car = Car("DA8818BB", "Renault Megane Grandtour", 2007, 25.0, "caravan")
+    val van1 = CargoVan("DA0009PO", "Volkswagen Transporter", 2018, 28.0, 2.0, 3, 4500.0)
+    val van2 = PassengerVan("DA6282EA", "IMV 1600", 1978, 35.0, 2.0,3, 0)
+    val limo = Limo("DA2238AB", "Zastava 750 LE", 1983, 220.0, 3.2, miniBar = false, sunRoof = false)
 
     println(v)
+    println(car)
+    println(van1)
+    println(van2)
+    println(limo)
 }
